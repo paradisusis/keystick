cd54f2438a173eaec3dd316c7157285c0f626508
Victor <paradisusvic@gmail.com>
2024-06-17 13:23:21 -0400

    Add project files

--- KeyStick/MainForm.cs
+++ KeyStick/MainForm.cs
@@ -2,90 +2,190 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
+using ParadisusIs;
 
 namespace KeyStick
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
-			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
-		}
-		
-		void OnKeyPressCheckBoxCheckedChanged(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnRefreshButtonClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnMainFormLoad(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnMainFormFormClosing(object sender, FormClosingEventArgs e)
-		{
-			
-		}
-		
-		void OnNewToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnMinimizeOnLoopStartToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
-		{
-			
-		}
-		
-		void OnFreeReleasesParadisusisToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnExitToolStripMenuItemClick(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void OnKeyPressTimerTick(object sender, EventArgs e)
-		{
-			
-		}
-	}
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowText", ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+
+        [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows", ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool EnumDesktopWindows(IntPtr hDesktop, EnumDelegate lpEnumCallbackFunction, IntPtr lParam);
+
+        private delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+
+        private const int MOD_CONTROL = 0x0002;
+
+        private const int WM_HOTKEY = 0x0312;
+
+        [DllImport("User32")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("User32")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        /// <summary>
+        /// Gets or sets the associated icon.
+        /// </summary>
+        /// <value>The associated icon.</value>
+        private Icon associatedIcon = null;
+
+        /// <summary>
+        /// The settings data.
+        /// </summary>
+        private SettingsData settingsData = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:KeyStick.MainForm"/> class.
+        /// </summary>
+        public MainForm()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the key press check box checked changed.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnKeyPressCheckBoxCheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the refresh button click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnRefreshButtonClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the main form load.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnMainFormLoad(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the main form form closing.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnMainFormFormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the new tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnNewToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the minimize on loop start tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnMinimizeOnLoopStartToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the options tool strip menu item drop down item clicked.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the free releases paradisusis tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnFreeReleasesParadisusisToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the original thread donation codercom tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the source code githubcom tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the about tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the exit tool strip menu item click.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnExitToolStripMenuItemClick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Handles the key press timer tick.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">E.</param>
+        private void OnKeyPressTimerTick(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
---
+++ KeyStick/SettingsData.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace ParadisusIs
+{
+    /// <summary>
+    /// Settings data.
+    /// </summary>
+    public class SettingsData
+    {
+        /// <summary>
+        /// Gets or sets the settings data path.
+        /// </summary>
+        /// <value>The settings data path.</value>
+        public string SettingsDataPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:ParadisusIs.SettingsData"/> always on top.
+        /// </summary>
+        /// <value><c>true</c> if always on top; otherwise, <c>false</c>.</value>
+        public bool AlwaysOnTop { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:ParadisusIs.SettingsData"/> add quotes.
+        /// </summary>
+        /// <value><c>true</c> if add quotes; otherwise, <c>false</c>.</value>
+        public bool AddQuotes { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets the copy program path.
+        /// </summary>
+        /// <value>The copy program path.</value>
+        public string CopyProgramPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the copy program arguments.
+        /// </summary>
+        /// <value>The copy program arguments.</value>
+        public string CopyProgramArguments { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:ParadisusIs.SettingsData"/> copy program hide.
+        /// </summary>
+        /// <value><c>true</c> if copy program hide; otherwise, <c>false</c>.</value>
+        public bool CopyProgramHide { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the move program path.
+        /// </summary>
+        /// <value>The move program path.</value>
+        public string MoveProgramPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the move program arguments.
+        /// </summary>
+        /// <value>The move program arguments.</value>
+        public string MoveProgramArguments { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:ParadisusIs.SettingsData"/> move program hide.
+        /// </summary>
+        /// <value><c>true</c> if move program hide; otherwise, <c>false</c>.</value>
+        public bool MoveProgramHide { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the delete program path.
+        /// </summary>
+        /// <value>The delete program path.</value>
+        public string DeleteProgramPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the delete program arguments.
+        /// </summary>
+        /// <value>The delete program arguments.</value>
+        public string DeleteProgramArguments { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:ParadisusIs.SettingsData"/> delete program hide.
+        /// </summary>
+        /// <value><c>true</c> if delete program hide; otherwise, <c>false</c>.</value>
+        public bool DeleteProgramHide { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the rename program path.
+        /// </summary>
+        /// <value>The rename program path.</value>
+        public string RenameProgramPath { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rename program arguments.
+        /// </summary>
+        /// <value>The rename program arguments.</value>
+        public string RenameProgramArguments { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:ParadisusIs.SettingsData"/> rename program hide.
+        /// </summary>
+        /// <value><c>true</c> if rename program hide; otherwise, <c>false</c>.</value>
+        public bool RenameProgramHide { get; set; } = false;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:PublicDomain.SettingsData"/> class.
+        /// </summary>
+        public SettingsData()
+        {
+            // Parameterless constructor
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:PublicDomain.SettingsData"/> class.
+        /// </summary>
+        /// <param name="settingsDataPath">Settings data path.</param>
+        /// <param name="newFile">If set to <c>true</c> new file.</param>
+        public SettingsData(string settingsDataPath, bool newFile)
+        {
+            // Set settings data path
+            this.SettingsDataPath = settingsDataPath;
+
+            // New settings file
+            if (newFile || !File.Exists(this.SettingsDataPath))
+            {
+                // Create new settings file
+                this.SaveSettingsFile();
+            }
+
+            /* Set all properties */
+
+            // TODO Check new file flag [Constructor flow can be improved]
+            if (newFile)
+            {
+                // Halt flow
+                return;
+            }
+
+            // Load from file
+            SettingsData settingsData = LoadSettingsFile();
+
+            // Iterate writable properties
+            foreach (PropertyInfo property in typeof(SettingsData).GetProperties().Where(p => p.CanWrite))
+            {
+                // Populate
+                property.SetValue(this, property.GetValue(settingsData, null), null);
+            }
+        }
+
+        /// <summary>
+        /// Loads the settings file.
+        /// </summary>
+        /// <returns>The settings file.</returns>
+        public SettingsData LoadSettingsFile()
+        {
+            // Use file stream
+            using (FileStream fileStream = File.OpenRead(this.SettingsDataPath))
+            {
+                // Set xml serialzer
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(SettingsData));
+
+                // Return populated settings data
+                return xmlSerializer.Deserialize(fileStream) as SettingsData;
+            }
+        }
+
+        /// <summary>
+        /// Saves the settings file.
+        /// </summary>
+        public void SaveSettingsFile()
+        {
+            try
+            {
+                // Check there's a settings data path to work with
+                if (this.SettingsDataPath == string.Empty)
+                {
+                    // Halt flow and advise
+                    throw new Exception("Empty settings data path.");
+                }
+
+                // Use stream writer
+                using (StreamWriter streamWriter = new StreamWriter(this.SettingsDataPath, false))
+                {
+                    // Set xml serialzer
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(SettingsData));
+
+                    // Serialize settings data
+                    xmlSerializer.Serialize(streamWriter, this);
+                }
+            }
+            catch (Exception exception)
+            {
+                // TODO Advise user [Can log to file to keep silent]
+                MessageBox.Show($"Error saving settings file.{Environment.NewLine}{Environment.NewLine}Message:{Environment.NewLine}{exception.Message}", "File error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
